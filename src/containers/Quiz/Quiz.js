40a921b12413399eeef255691f2f969632e7dc80
antonS <so4usertrue@gmail.com>
2019-05-17 02:30:19 +0300

    Block 6 less 9

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -6,6 +6,7 @@ export default class Quiz extends React.Component {
 
     state = {
         activeQuestion: 0,
+        answerState   : null,
         quiz          : [
             {
                 id           : 1,
@@ -33,10 +34,32 @@ export default class Quiz extends React.Component {
     };
 
     onAnswerClickHandler = (answerId) => {
-        console.log('Answer id: ', answerId);
-        this.setState({
-            activeQuestion: this.state.activeQuestion + 1
-        })
+        const question = this.state.quiz[this.state.activeQuestion];
+
+        if (question.rightAnswerId === answerId) {
+            this.setState({
+                answerState: {[answerId]: 'success'}
+            });
+            const timeout = window.setTimeout(() => {
+                if (this.isQuizFinished()) {
+                    console.log('Finished');
+                } else {
+                    this.setState({
+                        activeQuestion: this.state.activeQuestion + 1,
+                        answerState   : null
+                    })
+                }
+                window.clearTimeout(timeout);
+            }, 1000);
+        } else {
+            this.setState({
+                answerState: {[answerId]: 'error'}
+            });
+        }
+    };
+
+    isQuizFinished() {
+        return this.state.activeQuestion + 1 === this.state.quiz.length;
     }
 
     render() {
@@ -50,6 +73,7 @@ export default class Quiz extends React.Component {
                         onAnswerClick={this.onAnswerClickHandler}
                         quizLength={this.state.quiz.length}
                         answerNumber={this.state.activeQuestion + 1}
+                        state={this.state.answerState}
                     />
                 </div>
             </div>
