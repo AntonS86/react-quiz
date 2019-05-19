b88e625a73d31afcddf3c4dc8a56f22af2cb4773
antonS <so4usertrue@gmail.com>
2019-05-19 03:25:52 +0300

    Block 8 less 2

---
+++ src/containers/QuizCreater/QuizCreater.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default class QuizCreater extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Quiz Creater</h1>
+            </div>
+        )
+    }
+}
