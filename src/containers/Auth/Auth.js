241f2fb2c6dac8459d086b24e2b1c65f5b7ad207
antonS <so4usertrue@gmail.com>
2019-05-20 13:04:09 +0300

    Block 9 less 2

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -5,6 +5,35 @@ import Input from '../../components/UI/Input/Input';
 
 export default class Auth extends React.Component {
 
+    state = {
+        formControls: {
+            email   : {
+                value       : '',
+                type        : 'email',
+                label       : 'Email',
+                errorMessage: 'Введите корректный Email',
+                valid       : false,
+                touched     : false,
+                validation  : {
+                    required: true,
+                    email   : true,
+                },
+            },
+            password: {
+                value       : '',
+                type        : 'password',
+                label       : 'Пароль',
+                errorMessage: 'Введите корректный пароль',
+                valid       : false,
+                touched     : false,
+                validation  : {
+                    required : true,
+                    minLength: 6
+                }
+            }
+        }
+    }
+
     loginHandler = () => {
 
     }
@@ -17,16 +46,36 @@ export default class Auth extends React.Component {
         event.preventDefault();
     }
 
+    onChangeHandler = (event, controlName) => {
+        console.log(`${controlName}: `, event.target.value);
+    }
+
+    renderInputs() {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName];
+            return (
+                <Input
+                    key={controlName + index}
+                    type={control.type}
+                    value={control.value}
+                    valid={control.valid}
+                    touched={control.touched}
+                    label={control.label}
+                    shouldValidate={!!control.validation}
+                    errorMessage={control.errorMessage}
+                    onChange={(event) => this.onChangeHandler(event, controlName)}
+                />
+            )
+        });
+    }
+
     render() {
         return (
             <div className={classes.Auth}>
                 <div>
                     <h1>Авторизация</h1>
                     <form action="" onSubmit={this.submitHandler} className={classes.AuthForm}>
-                        <Input label="Email"/>
-                        <Input label="Пароль"
-                               errorMessage={'test'}
-                        />
+                        {this.renderInputs()}
                         <Button
                             type="success"
                             onClick={this.loginHandler}
