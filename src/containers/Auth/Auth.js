0bb8936d2c771ff2d10bddd3c9398308dcaeb4c1
antonS <so4usertrue@gmail.com>
2019-05-22 12:39:57 +0300

    Block 10 less 6

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import classes from './Auth.module.css';
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
 import is from 'is_js';
+import axios from 'axios'
 
 
 export default class Auth extends React.Component {
@@ -37,12 +38,36 @@ export default class Auth extends React.Component {
         }
     }
 
-    loginHandler = () => {
+    loginHandler = async () => {
+        const authData = {
+            email            : this.state.formControls.email.value,
+            password         : this.state.formControls.password.value,
+            returnSecureToken: true
+        }
+        try {
 
+            const response = await axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDlcRN2TxqmrHyBO5SvsyXqQuo6nj2nGaY',
+                authData)
+            console.log(response.data)
+        } catch (e) {
+            console.log(e)
+        }
     }
 
-    registerHandler = () => {
+    registerHandler = async () => {
+        const authData = {
+            email            : this.state.formControls.email.value,
+            password         : this.state.formControls.password.value,
+            returnSecureToken: true
+        }
+        try {
 
+            const response = await axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDlcRN2TxqmrHyBO5SvsyXqQuo6nj2nGaY',
+                authData)
+            console.log(response.data)
+        } catch (e) {
+            console.log(e)
+        }
     }
 
     submitHandler = event => {
